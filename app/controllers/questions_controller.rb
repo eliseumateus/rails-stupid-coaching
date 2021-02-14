317c7384009dacdee2f4ff7367d03712b7f6d33f
Eliseu Mateus <eliseumateus@gmail.com>
2021-02-14 20:38:12 +0000

    Created app and it's working. No help needed.

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,21 @@
+class QuestionsController < ApplicationController
+
+  def home
+    @todaysDate = Date.today.strftime("%B #{Date.today.day.ordinalize}")
+
+  end
+
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    if @question === 'I am going to work'
+      @answer =  'Great!'
+    elsif @question.include? '?'
+      @answer =  'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
